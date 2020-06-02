049c3a97a44dc8a9fc5ac72db7707a8a736cc3d7
Gennady7302 <63780048+Gennady7302@users.noreply.github.com>
2020-06-02 22:30:19 +0400

    48

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -21,23 +21,27 @@ let initialState = {
   };
 
 const dialogsReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case UPDATE_NEW_MESSAGE_BODY: {
-          let stateCopy = {...state}
-            stateCopy.newMessageBody = action.body;
-            return stateCopy;
-        }
-        case SEND_MESSAGE: {
-          let stateCopy = {...state};
-          stateCopy.messages = [...state.messages]
-            let body = stateCopy.newMessageBody;
-            stateCopy.newMessageBody = '';
-            stateCopy.messages.push({ id: 6, message: body });
-            return stateCopy;
-        }
-        default:
-            return state;
-    }
+
+  let stateCopy;
+
+  switch (action.type) {
+    case UPDATE_NEW_MESSAGE_BODY: 
+      return {
+        ...state,
+        newMessageBody: action.body
+      };      
+    
+    case SEND_MESSAGE: 
+      let body = state.newMessageBody;
+      return {
+        ...state,
+        newMessageBody: '',
+        messages: [...state.messages, { id: 6, message: body }]
+      };                  
+      
+    default:
+      return state;
+  }
 
 }
 
