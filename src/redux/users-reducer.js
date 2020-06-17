5d85f442a5019df4fc7a6a4413e888f2497e4484
Gennady7302 <63780048+Gennady7302@users.noreply.github.com>
2020-06-17 22:52:38 +0400

    55

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -2,11 +2,12 @@ const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
 const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
     users: [],
     pageSize: 5,
-    totalUsersCount: 21,
+    totalUsersCount: 0,
     currentPage: 1
   };
 
@@ -42,6 +43,10 @@ const usersReducer = (state = initialState, action) => {
         return { ...state, currentPage: action.currentPage}
       }
 
+      case SET_TOTAL_USERS_COUNT: {
+        return { ...state, totalUsersCount: action.count}
+      }
+
     default:
       return state;
   }
@@ -52,6 +57,7 @@ export const followAC = (userId) => ({type: FOLLOW, userId})
 export const unfollowAC = (userId) => ({type: UNFOLLOW, userId})
 export const setUsersAC = (users) => ({type: SET_USERS, users})
 export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
+export const setUsersTotalCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount})
 
     
 
