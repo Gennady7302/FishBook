be92a5963eca284fd4d52dc339b420faf6142d7f
Gennady7302 <8422201122@mail.ru>
2020-06-29 16:20:59 +0400

    62 of

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -33,10 +33,34 @@ let Users = (props) => {
                     <div>
                         {u.followed
                             ? <button onClick={() => {
-                                props.unfollow(u.id)
+
+                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY" : "77dd9349-9c18-4561-b804-5e6e517bb30e"
+                                    }
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode == 0) {
+                                            props.unfollow(u.id);
+                                        }
+                                    }); 
+
                             }}>Unfollow</button>
                             : <button onClick={() => {
-                                props.follow(u.id)
+
+                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                                    withCredentials: true,
+                                    headers: {
+                                        "API-KEY" : "77dd9349-9c18-4561-b804-5e6e517bb30e"
+                                    }
+                                })
+                                    .then(response => {
+                                        if (response.data.resultCode == 0) {
+                                            props.follow(u.id);
+                                        }
+                                    });                                
+
                             }}>Follow</button>}
 
                     </div>
