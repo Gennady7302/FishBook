c4a50a949618443947a3817c07d121c08ef56491
Gennady7302 <8422201122@mail.ru>
2020-07-08 15:45:50 +0400

    66 of

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,6 +3,7 @@ import styles from "./users.module.css";
 import userPhoto from "../../assets/images/user.jpg";
 import { NavLink } from 'react-router-dom';
 import * as axios from "axios"; 
+import { usersAPI } from '../../assets/images/api/api';
 
 let Users = (props) => {
 
@@ -34,38 +35,11 @@ let Users = (props) => {
                     <div>
                         {u.followed
                             ? <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                                props.toggleFollowingProgress(true, u.id);
-
-                                axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
-                                    withCredentials: true,
-                                    headers: {
-                                        "API-KEY" : "77dd9349-9c18-4561-b804-5e6e517bb30e"
-                                    }
-                                })
-                                    .then(response => {
-                                        if (response.data.resultCode == 0) {
-                                            props.unfollow(u.id);
-                                        }
-                                        props.toggleFollowingProgress(false, u.id);
-                                    }); 
-
+                                props.unfollow(u.id); 
                             }}>Unfollow</button>
+
                             : <button disabled={props.followingInProgress.some(id => id === u.id)} onClick={() => {
-                                props.toggleFollowingProgress(true, u.id);
-
-                                axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
-                                    withCredentials: true,
-                                    headers: {
-                                        "API-KEY" : "77dd9349-9c18-4561-b804-5e6e517bb30e"
-                                    }
-                                })
-                                    .then(response => {
-                                        if (response.data.resultCode == 0) {
-                                            props.follow(u.id);
-                                        }
-                                        props.toggleFollowingProgress(false, u.id);
-                                    });                                
-
+                                props.follow(u.id);  
                             }}>Follow</button>}
 
                     </div>
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import {connect} from "react-redux";
-import {toggleFollowingProgress, follow, setCurrentPage, setUsers, setTotalUsersCount, unfollow, toggleIsFetching} from "../../redux/users-reducer";
+import {
+    toggleFollowingProgress,
+    follow,
+    setCurrentPage,
+    unfollow,
+    getUsers
+} from "../../redux/users-reducer";
 
 import Users from "./Users";
 import Preloader from '../common/Preloader/Preloader';
@@ -9,25 +15,11 @@ import { usersAPI } from '../../assets/images/api/api';
 
 class UsersContainer extends React.Component {
     componentDidMount() {
-        this.props.toggleIsFetching(true);
-
-        usersAPI.getUsers(this.props.currentPage, this.props.pageSize)
-            .then(data => {
-                this.props.toggleIsFetching(false);
-                this.props.setUsers(data.items);
-                this.props.setTotalUsersCount(data.totalCount);
-            });
+        this.props.getUsers(this.props.currentPage, this.props.pageSize);        
     }
 
     onPageChanged = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber);
-        this.props.toggleIsFetching(true);
-
-        usersAPI.getUsers(pageNumber, this.props.pageSize)
-            .then(data => {
-                this.props.toggleIsFetching(false);
-                this.props.setUsers(data.items);
-            });
+        this.props.getUsers(pageNumber, this.props.pageSize);        
     }
 
     render() {
@@ -41,7 +33,6 @@ class UsersContainer extends React.Component {
                 users={this.props.users}
                 follow={this.props.follow}
                 unfollow={this.props.unfollow}
-                toggleFollowingProgress={this.props.toggleFollowingProgress}
                 followingInProgress={this.props.followingInProgress}
             />
         </>
@@ -62,9 +53,7 @@ let mapStateToProps = (state) => {
 export default connect(mapStateToProps, {
         follow,
         unfollow,
-        setUsers,
-        setCurrentPage,
-        setTotalUsersCount,
-        toggleIsFetching,
-        toggleFollowingProgress
+        setCurrentPage,        
+        toggleFollowingProgress,
+        getUsers
     })(UsersContainer);
