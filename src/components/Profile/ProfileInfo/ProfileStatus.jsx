e4bc4ffe93d3789057978e1539d187d7dfbccadb
Gennady7302 <8422201122@mail.ru>
2020-07-15 16:36:28 +0400

    72of

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -10,7 +10,7 @@ class ProfileStatus extends React.Component {
 
     }
 
-    activateEditMode () {
+    activateEditMode = () => {
         this.setState({
             editMode: true
         }) 
@@ -27,7 +27,7 @@ class ProfileStatus extends React.Component {
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={this.activateEditMode.bind(this)} >{this.props.status}</span>
+                        <span onDoubleClick={this.activateEditMode} >{this.props.status}</span>
                     </div>
                 }
                 {this.state.editMode &&
