4af7fbf10688413de6c48343c26f61401c501982
Gennady7302 <63780048+Gennady7302@users.noreply.github.com>
2020-09-10 21:32:47 +0400

    Updates

---
+++ precache-manifest.122146b4e9041d9a41812664131e3f74.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902ff76786609499827fa45e93c43085",
+    "url": "/FishBook/index.html"
+  },
+  {
+    "revision": "9c6952a070d23ac7e72a",
+    "url": "/FishBook/static/css/3.60fbe68e.chunk.css"
+  },
+  {
+    "revision": "294e81644c25c5c5b814",
+    "url": "/FishBook/static/css/4.c68e3f72.chunk.css"
+  },
+  {
+    "revision": "fec9f94bb59057a5ae12",
+    "url": "/FishBook/static/css/main.364e59dd.chunk.css"
+  },
+  {
+    "revision": "a37f332dcd1834d6177e",
+    "url": "/FishBook/static/js/2.bc278675.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/FishBook/static/js/2.bc278675.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c6952a070d23ac7e72a",
+    "url": "/FishBook/static/js/3.574d0013.chunk.js"
+  },
+  {
+    "revision": "294e81644c25c5c5b814",
+    "url": "/FishBook/static/js/4.28f1dce5.chunk.js"
+  },
+  {
+    "revision": "fec9f94bb59057a5ae12",
+    "url": "/FishBook/static/js/main.538706ec.chunk.js"
+  },
+  {
+    "revision": "a328708343e83a256412",
+    "url": "/FishBook/static/js/runtime-main.4deb8d7c.js"
+  },
+  {
+    "revision": "215d2729880b1c3ab4b44e530945887f",
+    "url": "/FishBook/static/media/user.215d2729.jpg"
+  }
+]);
